8c023dc84640ced4cffa7e61261c26e18e662691
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-09-13 21:53:58 +0300

    add sidebar

--- react-mui/src/components/Sidebar.jsx
+++ react-mui/src/components/Sidebar.jsx
@@ -1,5 +1,19 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import PeopleAltRoundedIcon from "@mui/icons-material/PeopleAltRounded";
+import Groups2RoundedIcon from "@mui/icons-material/Groups2Rounded";
+import SettingsSuggestRoundedIcon from "@mui/icons-material/SettingsSuggestRounded";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 const Sidebar = () => {
   return (
@@ -9,7 +23,60 @@ const Sidebar = () => {
       padding={2}
       sx={{ display: { xs: "none", sm: "block" } }}
     >
-      Sidebar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="home">
+            <ListItemIcon>
+              <HomeRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="home">
+            <ListItemIcon>
+              <AccountCircleRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="home">
+            <ListItemIcon>
+              <PeopleAltRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="home">
+            <ListItemIcon>
+              <Groups2RoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="home">
+            <ListItemIcon>
+              <SettingsSuggestRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="home">
+            <ListItemIcon>
+              <DarkModeIcon />
+            </ListItemIcon>
+            <Switch />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
