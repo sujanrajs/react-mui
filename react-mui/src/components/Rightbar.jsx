1859418527ff5ef68b3f43605e2731eaf892e733
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-09-13 20:44:26 +0300

    add break points in components

--- react-mui/src/components/Rightbar.jsx
+++ react-mui/src/components/Rightbar.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import { Box } from "@mui/material";
 
 const Rightbar = () => {
-  return <Box bgcolor="blue" flex={2} padding={2}>Rightbar</Box>;
+  return (
+    <Box
+      bgcolor="blue"
+      flex={2}
+      padding={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
+      Rightbar
+    </Box>
+  );
 };
 
 export default Rightbar;
--- react-mui/src/components/Sidebar.jsx
+++ react-mui/src/components/Sidebar.jsx
@@ -3,7 +3,12 @@ import { Box } from "@mui/material";
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="red" flex={1} padding={2}>
+    <Box
+      bgcolor="red"
+      flex={1}
+      padding={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
       Sidebar
     </Box>
   );
